3e1e8d0838f1de4c271a03baed47dbe3403e292a
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-08-09 22:15:33 -0300

    Atualização

---
+++ Primeiro_codigo/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+    Dim strMeuNome As String
+    Private strNome As String = "Carlito"
+    Dim intValor As Integer = 2
+    Public dblValor As Double
+    Const strName As String = "Barão de mauá"
+    Sub Main()
+        Dim strTipo As String = String.Empty
+        Console.WriteLine("Olá Mundo")
+        Console.WriteLine("Digite seu nome:")
+        strMeuNome = Console.ReadLine()
+        Console.WriteLine("Olá " + strMeuNome)
+        Console.ReadLine()
+    End Sub
+
+End Module
+
+Module Module2
+    Sub Escopo2()
+        Module1.dblValor = 5
+    End Sub
+End Module
---
+++ Primeiro_codigo/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associadas a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("Primeiro_codigo")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Primeiro_codigo")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("da0daa22-3c2b-4a7a-be9d-9cc1ef1de68f")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' usando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
