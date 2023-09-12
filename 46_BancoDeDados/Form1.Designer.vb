f81a4c823c3ac04ba58825e24f79b3bdf178b643
RicardoMoreiraPro <123956520+RicardoMoreiraPro@users.noreply.github.com>
2023-09-11 22:45:52 -0300

    Atualização

---
+++ 46_BancoDeDados/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AgendaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.tsb_cliente = New System.Windows.Forms.ToolStripButton()
+        Me.tsb_consultar = New System.Windows.Forms.ToolStripButton()
+        Me.ms_cadastro = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ms_consulta = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AgendaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(667, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AgendaToolStripMenuItem
+        '
+        Me.AgendaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ms_cadastro, Me.ms_consulta})
+        Me.AgendaToolStripMenuItem.Name = "AgendaToolStripMenuItem"
+        Me.AgendaToolStripMenuItem.Size = New System.Drawing.Size(60, 20)
+        Me.AgendaToolStripMenuItem.Text = "Agenda"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.tsb_cliente, Me.tsb_consultar})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 24)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(667, 39)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'tsb_cliente
+        '
+        Me.tsb_cliente.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsb_cliente.Image = Global._46_BancoDeDados.My.Resources.Resources.local_na_rede_internet
+        Me.tsb_cliente.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.tsb_cliente.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsb_cliente.Name = "tsb_cliente"
+        Me.tsb_cliente.Size = New System.Drawing.Size(36, 36)
+        Me.tsb_cliente.Text = "Cadastro Cliente"
+        '
+        'tsb_consultar
+        '
+        Me.tsb_consultar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.tsb_consultar.Image = Global._46_BancoDeDados.My.Resources.Resources.lupa
+        Me.tsb_consultar.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None
+        Me.tsb_consultar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.tsb_consultar.Name = "tsb_consultar"
+        Me.tsb_consultar.Size = New System.Drawing.Size(36, 36)
+        Me.tsb_consultar.Text = "Consultar"
+        '
+        'ms_cadastro
+        '
+        Me.ms_cadastro.Image = Global._46_BancoDeDados.My.Resources.Resources.local_na_rede_internet
+        Me.ms_cadastro.Name = "ms_cadastro"
+        Me.ms_cadastro.Size = New System.Drawing.Size(121, 22)
+        Me.ms_cadastro.Text = "Cadastro"
+        '
+        'ms_consulta
+        '
+        Me.ms_consulta.Image = Global._46_BancoDeDados.My.Resources.Resources.lupa
+        Me.ms_consulta.Name = "ms_consulta"
+        Me.ms_consulta.Size = New System.Drawing.Size(121, 22)
+        Me.ms_consulta.Text = "Consulta"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(667, 367)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Agenda"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents AgendaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ms_cadastro As ToolStripMenuItem
+    Friend WithEvents ms_consulta As ToolStripMenuItem
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents tsb_cliente As ToolStripButton
+    Friend WithEvents tsb_consultar As ToolStripButton
+End Class
---
+++ 46_BancoDeDados/frmConsulta.Designer.vb
@@ -0,0 +1,215 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmConsulta
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tb_nome = New System.Windows.Forms.TextBox()
+        Me.tb_cep = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.dgv_dados = New System.Windows.Forms.DataGridView()
+        Me.colCodigo = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colNome = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colCep = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColEndereco = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colBairro = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colComplemento = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colUF = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colFone1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.colFone2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.dgv_dados, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.tb_cep)
+        Me.GroupBox1.Controls.Add(Me.tb_nome)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(8, 2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(647, 100)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Filtrar por"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nome:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(456, 31)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "CEP:"
+        '
+        'tb_nome
+        '
+        Me.tb_nome.Location = New System.Drawing.Point(47, 28)
+        Me.tb_nome.Name = "tb_nome"
+        Me.tb_nome.Size = New System.Drawing.Size(384, 20)
+        Me.tb_nome.TabIndex = 2
+        '
+        'tb_cep
+        '
+        Me.tb_cep.Location = New System.Drawing.Point(490, 28)
+        Me.tb_cep.Name = "tb_cep"
+        Me.tb_cep.Size = New System.Drawing.Size(134, 20)
+        Me.tb_cep.TabIndex = 3
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox2.Controls.Add(Me.dgv_dados)
+        Me.GroupBox2.Location = New System.Drawing.Point(8, 108)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(803, 319)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        '
+        'dgv_dados
+        '
+        Me.dgv_dados.AllowUserToAddRows = False
+        Me.dgv_dados.AllowUserToDeleteRows = False
+        Me.dgv_dados.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgv_dados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_dados.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.colCodigo, Me.colNome, Me.colCep, Me.ColEndereco, Me.colBairro, Me.colComplemento, Me.colUF, Me.colFone1, Me.colFone2})
+        Me.dgv_dados.Location = New System.Drawing.Point(6, 10)
+        Me.dgv_dados.Name = "dgv_dados"
+        Me.dgv_dados.ReadOnly = True
+        Me.dgv_dados.Size = New System.Drawing.Size(791, 303)
+        Me.dgv_dados.TabIndex = 0
+        '
+        'colCodigo
+        '
+        Me.colCodigo.DataPropertyName = "CODIGO"
+        Me.colCodigo.HeaderText = "Código"
+        Me.colCodigo.Name = "colCodigo"
+        Me.colCodigo.ReadOnly = True
+        '
+        'colNome
+        '
+        Me.colNome.DataPropertyName = "NOME"
+        Me.colNome.HeaderText = "Nome"
+        Me.colNome.Name = "colNome"
+        Me.colNome.ReadOnly = True
+        '
+        'colCep
+        '
+        Me.colCep.DataPropertyName = "CEP"
+        Me.colCep.HeaderText = "CEP"
+        Me.colCep.Name = "colCep"
+        Me.colCep.ReadOnly = True
+        '
+        'ColEndereco
+        '
+        Me.ColEndereco.DataPropertyName = "ENDERECO"
+        Me.ColEndereco.HeaderText = "Endereço"
+        Me.ColEndereco.Name = "ColEndereco"
+        Me.ColEndereco.ReadOnly = True
+        '
+        'colBairro
+        '
+        Me.colBairro.DataPropertyName = "BAIRRO"
+        Me.colBairro.HeaderText = "Bairro"
+        Me.colBairro.Name = "colBairro"
+        Me.colBairro.ReadOnly = True
+        '
+        'colComplemento
+        '
+        Me.colComplemento.DataPropertyName = "COMPLEMENTO"
+        Me.colComplemento.HeaderText = "Complemento"
+        Me.colComplemento.Name = "colComplemento"
+        Me.colComplemento.ReadOnly = True
+        '
+        'colUF
+        '
+        Me.colUF.DataPropertyName = "UF"
+        Me.colUF.HeaderText = "UF"
+        Me.colUF.Name = "colUF"
+        Me.colUF.ReadOnly = True
+        '
+        'colFone1
+        '
+        Me.colFone1.DataPropertyName = "FONE1"
+        Me.colFone1.HeaderText = "Fone1"
+        Me.colFone1.Name = "colFone1"
+        Me.colFone1.ReadOnly = True
+        '
+        'colFone2
+        '
+        Me.colFone2.DataPropertyName = "FONE2"
+        Me.colFone2.HeaderText = "Fone2"
+        Me.colFone2.Name = "colFone2"
+        Me.colFone2.ReadOnly = True
+        '
+        'frmConsulta
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(823, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmConsulta"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Consulta"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        CType(Me.dgv_dados, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents tb_cep As TextBox
+    Friend WithEvents tb_nome As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents dgv_dados As DataGridView
+    Friend WithEvents colCodigo As DataGridViewTextBoxColumn
+    Friend WithEvents colNome As DataGridViewTextBoxColumn
+    Friend WithEvents colCep As DataGridViewTextBoxColumn
+    Friend WithEvents ColEndereco As DataGridViewTextBoxColumn
+    Friend WithEvents colBairro As DataGridViewTextBoxColumn
+    Friend WithEvents colComplemento As DataGridViewTextBoxColumn
+    Friend WithEvents colUF As DataGridViewTextBoxColumn
+    Friend WithEvents colFone1 As DataGridViewTextBoxColumn
+    Friend WithEvents colFone2 As DataGridViewTextBoxColumn
+End Class
